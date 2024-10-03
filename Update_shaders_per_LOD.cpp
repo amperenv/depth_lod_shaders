9b2e6741bdf52c55b5684bbdf16118200044ce5d
ampere_lowlevel <53409214+amperenv@users.noreply.github.com>
2024-10-03 07:52:03 +0300

    Add files via upload

---
+++ Update_shaders_per_LOD.cpp
@@ -0,0 +1,39 @@
+// Змінні для зберігання шейдерів
+VkPipeline closeLODShader;
+VkPipeline midLODShader;
+VkPipeline farLODShader;
+VkPipeline currentPipeline; // Curent graphic pipeline
+
+// Функція для вибору шейдера на основі фактора LOD
+void selectShaderBasedOnLOD()
+{
+    if (shaderConstants.FacNew == 1.0f)
+        currentPipeline = closeLODShader;
+    else if (shaderConstants.FacNew == 0.5f)
+        currentPipeline = midLODShader;
+    else
+        currentPipeline = farLODShader;
+}
+
+// Виклик у renderLoop
+void renderLoop()
+{
+    while (true)
+    {
+        float distance = calculateDistanceToCamera();
+
+        auto currentTime = std::chrono::steady_clock::now();
+        float timeSinceLastUpdate = std::chrono::duration<float>(currentTime - lastUpdateTime).count();
+        if (timeSinceLastUpdate > updateInterval)
+        {
+            updateShaderConstantsBasedOnDistance(distance);
+            selectShaderBasedOnLOD(); // Update shader
+        }
+
+        updateInterpolation();
+
+        // Використовуйте currentPipeline для малювання
+        vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, currentPipeline);
+        drawFrame();
+    }
+}
